101e71f73fb7add7c444d52212cfb07e4198c7d1
Caio Pessoa <caiopessoa1298@gmail.com>
2020-11-16 12:55:05 -0300

    Platform

--- App.js
+++ App.js
@@ -1,38 +1,7 @@
-import React from 'react';
-import { FlatList, StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { Platform, StyleSheet, Text } from 'react-native';
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        paddingTop: 22
-    },
-    item: {
-        padding: 10,
-        fontSize: 18,
-        height: 44,
-    }
-});
-
-const FlatListBasics = () => {
-    return (
-        <View style={styles.container}>
-            <FlatList
-            data={[
-                {key:'Devin'},
-                {key:'Dan'},
-                {key:'Dominic'},
-                {key:'Jackson'},
-                {key:'James'},
-                {key:'Joel'},
-                {key:'John'},
-                {key:'Jillian'},
-                {key:'Jimmy'},
-                {key:'Julie'},
-            ]}
-            renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
-            />
-        </View>
-    );
+const app = () => {
+    return <Text>{Platform.OS+ Platform.Version}</Text>
 }
-
-export default FlatListBasics
+export default app;
